cd5e447652f5e63155eec577e586c50873a46db6
itsninam <madalinaoancea@hotmail.com>
2022-04-01 14:53:07 -0400

    added new option to form and fetching data in js

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+const recipeApp = {};
+
+recipeApp.apiKey = "9f0deac75fc6469b92e2902b6660fd37";
+recipeApp.url = "https://api.spoonacular.com/food/search";
+
+recipeApp.getCusine = () => {
+  const formElement = document.querySelector("form");
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const userSelection = document.querySelector("select").value;
+    userSelection = "";
+
+    const url = new URL(recipeApp.url);
+    url.search = new URLSearchParams({
+      apiKey: recipeApp.apiKey,
+      number: 40,
+      query: userSelection,
+      // cuisine: "italian"
+    });
+
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((jsonRes) => {
+        console.log(jsonRes);
+        recipeApp.displayData(jsonRes);
+
+        // recipeApp.displayCusine(jsonResponse)
+      });
+  });
+};
